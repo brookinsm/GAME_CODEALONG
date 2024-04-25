e1160b96e8039837608a2f81990ab8c87e7b1d00
brookinsm <brookinsm@gmail.com>
2024-04-25 18:40:57 -0500

    continued ttt

--- js/main.js
+++ js/main.js
@@ -1,1 +1,76 @@
-console.log('main.js is loaded')
+/*------ Lookup Data / Constants -------*/
+const WINNING_COMBOS = [
+    [
+      { row: 0, col: 0 },
+      { row: 0, col: 1 },
+      { row: 0, col: 2 },
+    ],
+    [
+      { row: 1, col: 0 },
+      { row: 1, col: 1 },
+      { row: 1, col: 2 },
+    ],
+    [
+      { row: 2, col: 0 },
+      { row: 2, col: 1 },
+      { row: 2, col: 2 },
+    ],
+    [
+      { row: 0, col: 0 },
+      { row: 1, col: 0 },
+      { row: 2, col: 0 },
+    ],
+    [
+      { row: 0, col: 1 },
+      { row: 1, col: 1 },
+      { row: 2, col: 1 },
+    ],
+    [
+      { row: 0, col: 2 },
+      { row: 1, col: 2 },
+      { row: 2, col: 2 },
+    ],
+    [
+      { row: 0, col: 0 },
+      { row: 1, col: 1 },
+      { row: 2, col: 2 },
+    ],
+    [
+      { row: 0, col: 2 },
+      { row: 1, col: 1 },
+      { row: 2, col: 0 },
+    ],
+  ];
+  
+  /*------ State -------*/
+let board, turn, winner
+
+//   const board = [
+//     [null, null, null],
+//     [null, null, null],
+//     [null, null, null],
+//   ];
+
+//   let turn = 'x';
+//   let winner = null;
+
+  /*------ Logic -------*/
+ init()
+ function init() {
+ board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  turn = 'X'
+  winner = null
+
+    render();
+
+ }
+
+ function renter() {
+    rednerMessage()
+    renderBoard()
+ }
+  
